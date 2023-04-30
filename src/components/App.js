d180f46247388bde151f3398ddd6b9f5d32c4cd1
karuna sagar mishra <112199069+KARUNAMISHRA@users.noreply.github.com>
2023-04-30 15:42:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,33 @@ import React from 'react';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 import KeyValueStore from './KeyValueStore';
 import '../styles/App.css';
-
 function App() {
   return (
     <Router>
       <div id="main">
         <nav>
-          Reset
+          <button onClick={() => window.history.replaceState(null, null, '/')}>Reset</button>
+          
         </nav>
         <KeyValueStore />
       </div>
     </Router>
   );
 }
-
-
 export default App;
+
+
+
+  
+     
+        
+          
+        
+        
+      
+    
+  
+
+
+
+ 
