f565a303d4833333a2cd0337006a0b4598a268c8
karuna sagar mishra <112199069+KARUNAMISHRA@users.noreply.github.com>
2023-04-30 15:44:55 +0530

    Update KeyValueStore.js

--- src/components/KeyValueStore.js
+++ src/components/KeyValueStore.js
@@ -1,39 +1,77 @@
-import React, { useState, useEffect } from 'react';
+ import React, { useState, useEffect } from 'react';
 import '../styles/App.css';
+
 
 const KeyValueStore = () => {
   
-    const [keyValues, setKeyValues] = useState({});
+  const [keyValues, setKeyValues] = useState({});
+
   
-    const updateKeyValue = (key, value) => {};
+  const updateKeyValue = (key, value) => {
+    setKeyValues(prevState => {
+      return { ...prevState, [key]: value };
+    });
+  };
+
   
-    const handleUpdateClick = () => {};
+  const handleUpdateClick = () => {
+   
+    const queryParams = Object.keys(keyValues)
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(keyValues[key])}`)
+      .join('&');
+
   
-    const handleDeleteClick = (key) => {};
+    window.history.replaceState(null, null, `?${queryParams}`);
+  };
+
+ 
+  const handleDeleteClick = (key) => {
+    
+    setKeyValues(prevState => {
+      const newState = { ...prevState };
+      delete newState[key];
+      return newState;
+    });
+  };
+
   
-    useEffect(() => {}, []);
-  
-    return (
+  useEffect(() => {
+    const queryParams = new URLSearchParams(window.location.search);
+    const newKeyValues = {};
+
+    for (let [key, value] of queryParams.entries()) {
+      newKeyValues[decodeURIComponent(key)] = decodeURIComponent(value);
+    }
+
+    setKeyValues(newKeyValues);
+  }, []);
+
+  return (
+    <div>
+      <h1>Key Value Store</h1>
       <div>
-        <h1>Key Value Store</h1>
-          <div>
-            {/* Map the key-value using the below HTML */}
-              <div key={key} className='key-value-div'>
-                <span className='key-field'>key:</span>
-                <input
-                    className='value-field'
-                    type="text"
-                />
-                <button className='delete-btn'>Delete</button>
-              </div>
-
-            {/* Do not include Update button, while mapping the key-value */}
-            <button className='update-btn'>Update Values</button>
+        {/* map the key-value pairs from the state */}
+        {Object.entries(keyValues).map(([key, value]) => (
+          <div key={key} className='key-value-div'>
+            <span className='key-field'>{key}:</span>
+            <input
+              className='value-field'
+              type="text"
+              value={value}
+              onChange={(e) => updateKeyValue(key, e.target.value)}
+            />
+            <button className='delete-btn' onClick={() => handleDeleteClick(key)}>Delete</button>
           </div>
+        ))}
 
-          <p>No key values found in URL.</p>
+        
+        <button className='update-btn' onClick={handleUpdateClick}>Update Values</button>
       </div>
-    )
-}
+
+      
+      {Object.keys(keyValues).length === 0 && <p>No key values found in URL.</p>}
+    </div>
+  );
+};
 
 export default KeyValueStore;
